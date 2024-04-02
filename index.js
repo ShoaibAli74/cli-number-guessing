c8a403aa9c8ef42e360d81ec6f7c08b10b1d6579
Shoaib Ali <160453930+ShoaibAli74@users.noreply.github.com>
2024-04-03 02:19:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("welcome to a number guessing game");
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10: ",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guesses right number.");
+}
+else {
+    console.log("you guess wrong number");
+}
